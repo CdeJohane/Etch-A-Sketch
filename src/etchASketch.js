76a2b5b2f8d3e054c4e569c37016336ffcb8fa8c
Junior Johane <j.r.t.johane@gmail.com>
2022-12-20 01:26:51 +0200

    16*16 Squares Created

---
+++ src/etchASketch.js
@@ -0,0 +1,26 @@
+//Get Div of bigger Square
+let bigSquare = document.getElementById("squares");
+
+//constants for size and width of all squares
+const bigSide=350;
+const maxSquares = 16;
+let smallSide = Math.floor(bigSide/maxSquares);
+
+//Loop OFr Setting Up the squares
+for(let x=1; x<=maxSquares;x++){
+    //Create Divs for a row of maxSquares
+    let rowSquares = document.createElement("div");
+    for(let x=1; x<=maxSquares;x++){
+        //Create Div for each small Square
+        let smallSquare = document.createElement("div");
+        smallSquare.style.height=`${smallSide}px`;
+        smallSquare.style.width=`${smallSide}px`;
+        smallSquare.style.backgroundColor="#FFFFFF";
+        //Append smallSquare to the row
+        rowSquares.appendChild(smallSquare);
+    }
+    //Append Row to big square
+    bigSquare.appendChild(rowSquares); 
+}
+
+ 
