f0f663e752b699ee133ae20fd074fa341879f9db
Junior Johane <j.r.t.johane@gmail.com>
2022-12-20 02:08:35 +0200

    Variable Input of Squares Made

--- src/etchASketch.js
+++ src/etchASketch.js
@@ -1,28 +1,39 @@
 //Get Div of bigger Square
 let bigSquare = document.getElementById("squares");
 
+//Find size Button and when pressed, prompt for size
+const sizeBtn = document.getElementById("size");
+sizeBtn.addEventListener("click",function(e){
+    maxSquares = prompt("Enter Number Of Sides Of Square");
+    makeSquares();
+});
+
 //constants for size and width of all squares
 const bigSide=350;
-const maxSquares = 16;
-let smallSide = Math.floor(bigSide/maxSquares);
-
-//Loop OFr Setting Up the squares
-for(let x=1; x<=maxSquares;x++){
-    //Create Divs for a row of maxSquares
-    let rowSquares = document.createElement("div");
+let maxSquares = 16;
+makeSquares()
+function makeSquares(){
+    bigSquare.innerHTML="";
+    let smallSide = Math.floor(bigSide/maxSquares);
+    //Loop OFr Setting Up the squares
     for(let x=1; x<=maxSquares;x++){
-        //Create Div for each small Square
-        let smallSquare = document.createElement("div");
-        smallSquare.style.height=`${smallSide}px`;
-        smallSquare.style.width=`${smallSide}px`;
-        smallSquare.style.backgroundColor="#FFFFFF";
-        smallSquare.addEventListener("mouseover", function(e){
+        //Create Divs for a row of maxSquares
+        let rowSquares = document.createElement("div");
+        for(let x=1; x<=maxSquares;x++){
+            //Create Div for each small Square
+            let smallSquare = document.createElement("div");
+            smallSquare.style.height=`${smallSide}px`;
+            smallSquare.style.width=`${smallSide}px`;
+            smallSquare.style.backgroundColor="#FFFFFF";
+            smallSquare.addEventListener("mouseover", function(e){
             smallSquare.style.backgroundColor="#F2F300"
-        })
+        });
         //Append smallSquare to the row
         rowSquares.appendChild(smallSquare);
     }
     //Append Row to big square
     bigSquare.appendChild(rowSquares); 
 }
+}
+
  
